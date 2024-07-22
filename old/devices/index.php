90ebd58c8f7026c54f71a9cc83ccaf18d6419d5a
Am1nCmd <mail@am1ncmd.id>
2024-07-22 16:56:56 +0700

    remove old

--- old/devices/index.php
+++
@@ -1,9 +0,0 @@
-<?php header("Location: https://appledb.dev/device-selection/"); ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Devices Selection</title>
-   </head>
-   <body>
-   </body>
-</html>
--- old/sep-bb/index.php
+++
@@ -1,9 +0,0 @@
-<?php header("Location: https://docs.google.com/spreadsheets/d/1QjWyoDfaiF-TWhzVdvEMRqA3OQXsz6e8of3SxZB1W_M/edit?usp=sharing"); ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>SEP/BB Chart</title>
-   </head>
-   <body>
-   </body>
-</html>
